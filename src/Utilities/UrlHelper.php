08e7d253dc657c3910a5a905a1051a3074148f28
rcravens <bob.cravens@gmail.com>
2025-02-13 08:53:15 -0600

    added UrlHelper

---
+++ src/Utilities/UrlHelper.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Cravens\Php\Utilities;
+
+class UrlHelper
+{
+	public static function build( string $base_url, array $params ): string
+	{
+		if ( count( $params ) == 0 )
+		{
+			return $base_url;
+		}
+
+		$terms = [];
+		foreach ( $params as $key => $value )
+		{
+			$terms[] = $key . '=' . $value;
+		}
+
+		$new_query_string = implode( '&', $terms );
+
+		if ( str_contains( $base_url, '?' ) )
+		{
+			return $base_url . '&' . $new_query_string;
+		}
+
+		return $base_url . '?' . $new_query_string;
+	}
+}
